443382f900d1f602706331b835dea7af43d4aa4a
Nadav Michael <nadav203@gmail.com>
2019-09-28 07:39:03 +0000

    format

--- src/format.cpp
+++ src/format.cpp
@@ -4,8 +4,30 @@
 
 using std::string;
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
+// helper 
+// Input: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+  string time = "00:00:00";
+  long cal = 0;
+  if (seconds > 0) {
+    // calculate hours
+    cal = seconds / 3600;
+    time = TimeValueToString(cal) + ":";
+    // calculate minutes
+    cal = (seconds / 60) % 60;
+    time += TimeValueToString(cal) + ":";
+    // calculate seconds
+    cal = seconds % 60;
+    time += TimeValueToString(cal);
+  }
+
+  return time;
+}
+
+std::string Format::TimeValueToString(long time) {
+  if (time < 10)
+    return "0" + std::to_string(time);
+  else
+    return std::to_string(time);
+}
