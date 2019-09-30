9428e2d47ee242d38c6f04be510c829db22a119f
Nadav Michael <nadav203@gmail.com>
2019-09-30 18:24:26 +0000

    uid bug fix

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -285,7 +285,7 @@ string LinuxParser::Uid(int pid) {
     while (std::getline(filestream, line)) {
       std::istringstream linestream(line);
       while (linestream >> key >> value) {
-        if (key == "Uid") {
+        if (key == "Uid:") {
           user_id = value;
           return user_id;
         }
